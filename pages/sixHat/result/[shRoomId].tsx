17eed4adc32e5bcb049b671a8ba5ea545a34f027
yechan <noy3928@naver.com>
2022-03-26 19:13:07 +0900

    feat : make result page in sixhat

---
+++ pages/sixHat/result/[shRoomId].tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { GetServerSideProps } from 'next';
+import { ResultBox } from '../../../src/components/layout/SixHat';
+import { CenterLayout, PrimaryButton } from '@components/common';
+import { ResultModal } from '@components/common/Modals';
+import { useRouter } from 'next/router';
+import axios from 'axios';
+
+type ResultProps = {
+  shRoomId: string;
+};
+
+const Result = ({ shRoomId }: ResultProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const router = useRouter();
+
+  const handleComplete = () => {
+    setIsOpen(false);
+    router.push('/');
+  };
+
+  const handleCancel = () => {
+    axios.patch(`${process.env.NEXT_PUBLIC_API_URL}/api/sixHat/sharing/${shRoomId}`);
+  };
+
+  return (
+    <CenterLayout>
+      <>
+        <ResultBox />
+        <PrimaryButton text="완료" onClick={() => setIsOpen(true)} />
+        {isOpen && <ResultModal onClickBtn1={handleCancel} onClickBtn2={handleComplete} />}
+      </>
+    </CenterLayout>
+  );
+};
+
+export default Result;
+
+export const getServerSideProps: GetServerSideProps = async context => {
+  const { query } = context;
+  const { shRoomId } = query;
+  return {
+    props: {
+      shRoomId,
+    },
+  };
+};
---
+++ src/components/layout/SixHat/ResultBox/Message/Message.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import styled from 'styled-components';
+import { themedPalette } from '../../../../../theme/styleTheme';
+const HatSrc = require('../../../../../../public/asset/hat.png');
+import { HatType } from '../../../../../redux/modules/sixHat/types';
+import { HatImage } from '@components/common';
+
+type MessageProps = {
+  isMe: boolean;
+  message: string | null;
+  hatName: string | null;
+  hat: HatType;
+};
+
+type StyleProps = {
+  isMe: boolean;
+};
+
+const Message = ({ isMe, message, hatName, hat }: MessageProps) => {
+  return (
+    <OtherMessageBox>
+      <ProfileBox>
+        <HatImage type={hat} width={30} height={30} />
+        <HatName>{hatName}</HatName>
+      </ProfileBox>
+      <Box>{message}</Box>
+    </OtherMessageBox>
+  );
+};
+
+const MessageBox = styled.div<StyleProps>`
+  width: 100%;
+  display: flex;
+  justify-content: ${props => (props.isMe ? `flex-end` : 'flex-start')};
+  align-items: flex-start;
+  margin-bottom: 16px;
+`;
+
+const OtherMessageBox = styled.div`
+  display: flex;
+  gap: 24px;
+`;
+
+const HatImg = styled.img`
+  width: 50px;
+`;
+
+const ProfileBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const HatName = styled.span``;
+
+const Box = styled.div`
+  border: 5px solid ${themedPalette.border_1};
+  border-radius: 18px;
+  padding: 20px 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 406px;
+`;
+
+const MyMessageBox = styled.div``;
+
+export { Message };
